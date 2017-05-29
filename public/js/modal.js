1445b9e6bad079ba237f7d66659fa0ba41e71747
dimaer <dimaer613@gmail.com>
2017-05-29 20:46:41 +0200

    Implementazione di popup di promozione

---
+++ public/js/modal.js
@@ -0,0 +1,28 @@
+var clicked = false;
+$('.coupon-button').click(function() {
+	if(!clicked){
+		clicked=true;
+		console.log($(window).scrollTop());
+		$(this).parent().next().css("visibility","visible");
+		$(this).parent().next().css({
+			"width":"600px",
+			"height":"500px",
+			"top":$(window).scrollTop(),
+			"left":$(document).width()/2-300
+		}).find('.description').css({
+			"height":"300px"
+		});
+		$('.fa.fa-close.close-item-popup').click(function(){
+			$(this).parent().css({
+				"visibility":"hidden",
+				"width":"0px",
+				"height":"0px"
+			});
+			$(this).parent().find('.description').css({
+				"width":"0px",
+				"height":"0px"
+			})
+			clicked = false;
+		});
+	}
+});
